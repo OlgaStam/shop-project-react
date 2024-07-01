ad9b2e1c83eac9c1ec9af65ca28ea5bf16cfc0b6
OlgaStam <newrealityforme@gmail.com>
2024-07-01 11:12:35 +0300

    создание элементов с помощью JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// React Element
-const title = React.createElement('h1', { id: 'title' }, 'Hello Aloha!')
+// // React Element
+// const title = React.createElement('h1', { id: 'title' }, 'Hello Aloha!')
+// console.log('🚀 ~ title:', title)
+
+// JSX
+
+const app = <h1 id="text">Hello App</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
