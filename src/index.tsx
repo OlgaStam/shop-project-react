9945752a6c815b39588e43ce733c5ee7206c635a
OlgaStam <newrealityforme@gmail.com>
2024-07-01 14:05:46 +0300

    <React.Fragment>, <> - несколько компонентов без лишних уровней вложенности

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1> Hello Title component</h1>
+}
+const Content = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Delectus, ea.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Delectus, ea.
+            </p>
+        </>
+    )
+}
 const App = () => {
     return (
-        <div>
-            <h1> Hello App component</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Delectus, ea.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Delectus, ea.
-            </p>
-        </div>
+        <>
+            <Title />
+            <Content />
+        </>
     )
 }
 
