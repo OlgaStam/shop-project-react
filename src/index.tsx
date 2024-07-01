69bc56ae4a8b7f724936fbeaa035ac8dd3b9ac7c
OlgaStam <newrealityforme@gmail.com>
2024-07-01 13:51:52 +0300

    реализовали реакт-компонент

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// // React Element
-// const title = React.createElement('h1', { id: 'title' }, 'Hello Aloha!')
-// console.log('🚀 ~ title:', title)
-
-// JSX
-
-const app = (
-    <div>
-        <h1 id="text">Hello App</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
-            voluptas eaque sequi adipisci officia itaque nesciunt, vero
-            perspiciatis, corrupti enim omnis voluptate. Autem, illo eius?
-        </p>
-    </div>
-)
+const App = () => {
+    return (
+        <div>
+            <h1> Hello App component</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Delectus, ea.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Delectus, ea.
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
