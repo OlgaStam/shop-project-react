fdb3bfa2b01a84ceeefdbe3705e03443db1a0bb3
OlgaStam <newrealityforme@gmail.com>
2024-07-03 13:49:44 +0300

    повторение экспорт-импорт

---
+++ src/Content.tsx
@@ -0,0 +1,13 @@
+type contetntProps = {
+    p1: string
+    p2?: string
+}
+const Content = ({ p1, p2 = 'ybxtuj' }: contetntProps) => {
+    return (
+        <>
+            <p className="red">{p1}</p>
+            <p>{p2}</p>
+        </>
+    )
+}
+export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import Title from './Title'
+import Content from './Content'
 
-const Content = () => {
-    return (
-        <>
-            <p className="red">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Delectus, ea.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Delectus, ea.
-            </p>
-        </>
-    )
-}
 const App = () => {
     return (
         <>
             <Title title="App" />
             <Title title="React" num={1} />
-            <Content />
+            <Content p1="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, pariatur suscipit? Autem sit facere laborum harum tempore similique, voluptatem saepe enim cumque explicabo magni reprehenderit sequi molestias laboriosam. Id, illum quidem? Provident minima adipisci officia! Et dolorem enim quibusdam adipisci libero minima eligendi, doloremque harum excepturi magnam vero quos labore?" />
         </>
     )
 }
