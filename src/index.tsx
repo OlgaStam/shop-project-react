241fa6f85fbc39f69bface0cfd15e4f35e8504ed
OlgaStam <newrealityforme@gmail.com>
2024-07-03 14:03:51 +0300

    пришел дворник все подмел - осталась голая структура

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
-import Title from './Title'
-// import BlaBla from './Content' - дефолтный экспорт как хочешь назови
-import { Content } from './Content'
+
 const App = () => {
-    return (
-        <>
-            <Title title="App" />
-            <Title title="React" num={1} />
-
-            {/* дефолтный экспорт как хочешь назови */}
-            {/* <BlaBla p1="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, pariatur suscipit? Autem sit facere laborum harum tempore similique, voluptatem saepe enim cumque explicabo magni reprehenderit sequi molestias laboriosam. Id, illum quidem? Provident minima adipisci officia! Et dolorem enim quibusdam adipisci libero minima eligendi, doloremque harum excepturi magnam vero quos labore?" /> */}
-
-            <Content p1="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, pariatur suscipit? Autem sit facere laborum harum tempore similique, voluptatem saepe enim cumque explicabo magni reprehenderit sequi molestias laboriosam. Id, illum quidem? Provident minima adipisci officia! Et dolorem enim quibusdam adipisci libero minima eligendi, doloremque harum excepturi magnam vero quos labore?" />
-        </>
-    )
+    return <></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
