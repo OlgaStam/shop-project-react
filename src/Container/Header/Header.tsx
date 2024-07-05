49f6f733fff1834f80a2a753897d1ce9b73df495
OlgaStam <newrealityforme@gmail.com>
2024-07-05 12:09:44 +0300

    вынесла Logo - компонент

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -1,11 +1,11 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
-import logo from '../../assets/logo.svg'
+
+import Logo from 'components/Logo/Logo'
 import './Header.scss'
 
 type Props = {}
@@ -23,13 +23,7 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        <img src={logo} alt="Fake-shop logo" />
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
