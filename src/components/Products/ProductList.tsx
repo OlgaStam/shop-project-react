23c46afc3a58617a8801dd8ee51d036555cb6fca
OlgaStam <newrealityforme@gmail.com>
2024-07-09 16:40:05 +0300

    стили для карточки товаров ProductListItem

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,5 +1,15 @@
+import { Typography } from '@mui/material'
+import ProductListItem from './ProductListItem'
+
 type Props = {}
 const ProductList = (props: Props) => {
-    return <div>ProductList</div>
+    return (
+        <>
+            <Typography component="h2" variant="h3" align="center">
+                List of Products
+            </Typography>
+            <ProductListItem />
+        </>
+    )
 }
 export default ProductList
--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -3,16 +3,16 @@ import { Button, Card, CardActions, CardContent } from '@mui/material'
 type Props = {}
 const ProductListItem = (props: Props) => {
     return (
-        <Card className="producnt">
+        <Card className="producnt" variant="outlined">
             <CardContent>
                 <h4 className="product-title">iPfone X</h4>
-                <p className="product-discription">This is iPhone X</p>
+                <div className="product-discription">This is iPhone X</div>
                 <div className="product-features">Type: phone</div>
                 <div className="product-features">Capacity: 64Gb</div>
                 <div className="product-price">$ 500</div>
             </CardContent>
             <CardActions>
-                <Button>Add to card</Button>
+                <Button variant="outlined">Add to card</Button>
             </CardActions>
         </Card>
     )
