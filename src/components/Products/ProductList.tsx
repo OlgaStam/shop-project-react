4029e5268246cb09cd84b7c25d40dc24b474ace3
OlgaStam <newrealityforme@gmail.com>
2024-07-09 16:47:34 +0300

    добавили Grid

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import ProductListItem from './ProductListItem'
 
 type Props = {}
@@ -8,7 +8,23 @@ const ProductList = (props: Props) => {
             <Typography component="h2" variant="h3" align="center">
                 List of Products
             </Typography>
-            <ProductListItem />
+            <Grid container spacing={4}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem />
+                </Grid>
+            </Grid>
         </>
     )
 }
