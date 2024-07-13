5835325ec045ec6a3116cef2d53a58ce43840351
OlgaStam <newrealityforme@gmail.com>
2024-07-13 20:58:19 +0300

    базовый синтаксис useState

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -6,6 +6,7 @@ import {
     TextField,
 } from '@mui/material'
 import './ProductListItem.scss'
+import { useState } from 'react'
 
 type Props = {
     image: any
@@ -14,26 +15,41 @@ type Props = {
     capacity: string
     price: number
 }
-const ProductListItem = ({ image, title, type, capacity, price }: Props) => (
-    <Card className="product" variant="outlined">
-        <CardContent>
-            <div className="product-img">
-                <img src={image} alt="" />
-            </div>
-            <h4 className="product-title">{title}</h4>
-            <div className="product-discription">This is {title}</div>
-            <div className="product-features">Type: {type}</div>
-            <div className="product-features">Capacity: {capacity}Gb</div>
-            <div className="product-price">Price: $ {price}</div>
-            <div className="product-quantity">
-                <Button variant="outlined">-</Button>
-                <TextField value="1" size="small" />
-                <Button variant="outlined">+</Button>
-            </div>
-        </CardContent>
-        <CardActions className="btns-wrap">
-            <Button variant="outlined">Add to card</Button>
-        </CardActions>
-    </Card>
-)
+const ProductListItem = ({ image, title, type, capacity, price }: Props) => {
+    const [count, setCount] = useState<number>(2)
+    return (
+        <Card className="product" variant="outlined">
+            <CardContent>
+                <div className="product-img">
+                    <img src={image} alt="" />
+                </div>
+                <h4 className="product-title">{title}</h4>
+                <div className="product-discription">This is {title}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}Gb</div>
+                <div className="product-price">Price: $ {price}</div>
+                <div className="product-quantity">
+                    <Button
+                        variant="outlined"
+                        onClick={() => setCount(count - 1)}
+                        disabled={count === 1}
+                    >
+                        -
+                    </Button>
+                    <TextField value={count} size="small" />
+                    <Button
+                        variant="outlined"
+                        onClick={() => setCount(count + 1)}
+                        disabled={count === 10}
+                    >
+                        +
+                    </Button>
+                </div>
+            </CardContent>
+            <CardActions className="btns-wrap">
+                <Button variant="outlined">Add to card</Button>
+            </CardActions>
+        </Card>
+    )
+}
 export default ProductListItem
