23a3244a794770b268a6164de328e6e90dd54adc
OlgaStam <newrealityforme@gmail.com>
2024-07-14 12:04:07 +0300

    добавление в корзину
    предварительная кнопка в App

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -10,14 +10,29 @@ type CartData = {
 }
 const App = () => {
     const [cartData, setCartData] = useState<CartData>({
-        totalCount: 10,
-        totalPrice: 100,
+        totalCount: 0,
+        totalPrice: 0,
     })
+    const addProductToCart = (count: number, price: number) => {
+        setCartData((prevState) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + price * count,
+        }))
+    }
     return (
         <>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header cartData={cartData} />
+                <button
+                    style={{
+                        top: '80px',
+                        position: 'fixed',
+                    }}
+                    onClick={() => addProductToCart(5, 500)}
+                >
+                    add to cart (count:5, prise:500$)
+                </button>
                 <Main />
             </StyledEngineProvider>
         </>
