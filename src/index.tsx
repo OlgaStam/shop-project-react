efb8c149410f445446dbdd2e692b2c157c5db4d2
OlgaStam <newrealityforme@gmail.com>
2024-07-21 15:08:44 +0300

    стилизация пунктов меню
    шаг 1 создаем кастомный компонент для пункта меню
    шаг 2 в меню заменяем пункты на этот компонент
    шаг 3 добавляем стилей

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'Container/App/App'
 import { BrowserRouter } from 'react-router-dom'
-import { Global, css } from '@emotion/react'
-const globalStyles = css`
-    a {
-        text-decoration: none;
-        color: inherit; /* Наследует цвет от родительского элемента */
-    }
-    a:visited {
-        color: #fc95b5;
-    }
-    a.active {
-        color: #ff00cf;
-    }
-`
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <Global styles={globalStyles} />
         <BrowserRouter>
             <App />
         </BrowserRouter>
