a3f1a8f873934fb3d4c5cd4a332485f9e1d521cc
OlgaStam <newrealityforme@gmail.com>
2024-07-23 17:58:12 +0300

    устранение ошибок перед диплоем

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -2,20 +2,14 @@ import { Grid } from '@mui/material'
 import CartProductList from 'components/Cart/CartProdeuctList'
 import CartProductListItemExtended from 'components/Cart/CartProductListItemExtended'
 import CartTotal from 'components/Cart/CartTotal'
-import productsArray, { Product, getProductsObject } from 'utils/productsArray'
 
 type Props = {
-    productsInCart: productsInCart
-}
-type productsInCart = {
-    [id: number]: number
+    productsInCart: {
+        [id: number]: number
+    }
 }
 
-type productsObjectType = {
-    [id: number]: Product
-}
 const CartPage = ({ productsInCart }: Props) => {
-    const productsObject: productsObjectType = getProductsObject(productsArray)
     return (
         <>
             <div>
