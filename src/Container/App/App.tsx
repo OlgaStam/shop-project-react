076ca77c8232243fcb871dde06d8db0e297343ad
OlgaStam <newrealityforme@gmail.com>
2024-07-24 20:36:30 +0300

    Создание контекста
    Добавили AppContext с помощью метода removeProductFromCart и интегрируем в CartComponent.

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,5 +1,5 @@
 import Header from 'Container/Header/Header'
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from 'pages/Home/Home'
 import CartPage from 'pages/Cart/CartPage'
@@ -11,6 +11,12 @@ const theme = createTheme()
 type productsInCart = {
     [id: number]: number
 }
+
+type Context = {
+    removeProductFromCart: (id: number) => void
+}
+export const AppContext = createContext<Context | null>(null)
+
 const App = () => {
     const [productsInCart, setProductsInCart] = useState<productsInCart>({})
 
