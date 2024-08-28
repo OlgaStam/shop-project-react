a10093a9aaa45d53325efc9156bb578e53744c60
OlgaStam <newrealityforme@gmail.com>
2024-08-28 14:53:02 +0300

    Добавлен функционал отображения списка категорий и фильтрации по категории
    - Импортированы компоненты `Link` и `useParams` из `react-router-dom` для маршрутизации.
    - Извлечение уникальных категорий из массива `productsArray`.
    - Реализовано отображение списка категорий на странице при отсутствии `categoryId` в URL.
    - Реализовано отображение товаров по выбранной категории, если `categoryId` присутствует в URL.

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,7 +1,50 @@
-type Props = {}
+import { Link, useParams } from 'react-router-dom'
+import productsArray from 'utils/productsArray'
 
-const Category = (props: Props) => {
-    return <div>Category</div>
+const Category = () => {
+    const { categoryId } = useParams<{ categoryId?: string }>()
+
+    // Извлекаем уникальные категории из productsArray
+    const categories = productsArray
+        .filter((product) => product.category)
+        .reduce<string[]>((acc, product) => {
+            if (!acc.includes(product.category!)) {
+                acc.push(product.category!)
+            }
+            return acc
+        }, [])
+
+    if (!categoryId) {
+        return (
+            <div>
+                <h1>Categories</h1>
+                <ul>
+                    {categories.map((category) => (
+                        <li key={category}>
+                            {' '}
+                            <Link to={`/category/${category}`}>{category}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+
+    // Если categoryId существует, ищем соответствующую категорию
+    const filteredProducts = productsArray.filter(
+        (product) => product.category === categoryId
+    )
+
+    return (
+        <div>
+            <h1>Category: {categoryId}</h1>
+            <ul>
+                {filteredProducts.map((product) => (
+                    <li key={product.id}>{product.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Category
